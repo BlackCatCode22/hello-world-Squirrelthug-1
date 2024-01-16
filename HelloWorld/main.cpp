85571b3cb1f3abc73b7069f547cebc9c782e9d46
Ratatoskyr <63626948+Squirrelthug@users.noreply.github.com>
2024-01-16 12:42:38 -0800

    Add files via upload
    Hello World

---
+++ HelloWorld/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+    // Main() function: where the execution of
+    // program begins
+int main()
+{
+    // Prints "Hello World" to the terminal
+    std::cout << "Hello World";
+    return 0;
+}
